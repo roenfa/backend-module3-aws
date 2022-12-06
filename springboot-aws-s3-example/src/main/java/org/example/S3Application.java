e444d4255179cd7b99f5bd2dfe36b56c26bc42ae
Jesus Vallecilla <jesusvallecilla27@gmail.com>
2022-12-05 23:26:38 -0500

    feat: implement missing AWSS3Service integration tests

--- springboot-aws-s3-example/src/main/java/org/example/S3Application.java
+++ springboot-aws-s3-example/src/main/java/org/example/S3Application.java
@@ -19,18 +19,18 @@ public class S3Application {
         if (this.awsS3Service.isBucketExist(bucketName)) {
             System.out.println("Bucket name exists!!!");
         } else {
-            this.awsS3Service.createBucket(bucketName);
+//            this.awsS3Service.createBucket(bucketName);
 
             //uploading
             this.awsS3Service.uploadObject(bucketName,
-                    "Documents/files/bootcamp.txt",
+                    "test/bootcamp.txt",
                     new File("C:/Users/Gatito/Documents/bootcamp.txt"));
 
             // get object and copy in Desktop/bootcamp.txt
 
             //copying from s3 bucket 1 to s3 bucket 2
-            this.awsS3Service.copyObject("bootcamp-backend-bucket", "picture/pic.png",
-                    "bootcamp-backend-bucket2", "Documents/picture.png");
+//            this.awsS3Service.copyObject("bootcamp-backend-bucket", "picture/pic.png",
+//                    "bootcamp-backend-bucket2", "Documents/picture.png");
 
         }
     }
--- springboot-aws-s3-example/src/test/java/AWSS3ServiceIntegrationTest.java
+++ springboot-aws-s3-example/src/test/java/AWSS3ServiceIntegrationTest.java
@@ -1,22 +1,29 @@
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.*;
+import com.amazonaws.services.s3.model.DeleteObjectsRequest.KeyVersion;
+
 import org.example.services.AWSS3Service;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+
+import java.io.File;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.*;
 
 public class AWSS3ServiceIntegrationTest {
-    private final String BUCKET_NAME = "bucket_name";
+    private final String BUCKET_NAME = "bootcamp-backend-jesus";
+    private final String FILE_KEY = "test/bootcamp.txt";
     private AmazonS3 s3;
     private AWSS3Service service;
 
     @Before
     public void setUp() {
         s3 = mock(AmazonS3.class);
-        service = new AWSS3Service(s3);
+        service = spy(new AWSS3Service(s3));
     }
 
     @Test
@@ -31,5 +38,89 @@ public class AWSS3ServiceIntegrationTest {
         var result = this.service.isBucketExist(BUCKET_NAME);
         //verify(s3).doesBucketExistV2(BUCKET_NAME);
         Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void whenGetObjectFromS3Bucket_thenNotNull() {
+        this.service.getObject(BUCKET_NAME,FILE_KEY);
+
+        doReturn(new S3Object()).when(this.service).getObject(BUCKET_NAME, FILE_KEY);
+        verify(this.service).getObject(BUCKET_NAME, FILE_KEY);
+    }
+
+    @Test
+    public void whenCreateS3Bucket_thenCorrect() {
+        this.service.createBucket(BUCKET_NAME);
+
+        doReturn(new Bucket()).when(this.service).createBucket(BUCKET_NAME);
+        verify(this.service).createBucket(BUCKET_NAME);
+    }
+
+    @Test
+    public void whenUploadObjectToS3Bucket_thenCorrect() {
+        String filePath = "C:/Users/Gatito/Documents/bootcamp.txt";
+
+        this.service.uploadObject(
+                BUCKET_NAME,
+                FILE_KEY,
+                new File(filePath)
+        );
+
+        doReturn(new PutObjectResult())
+                .when(this.service)
+                .uploadObject(
+                        BUCKET_NAME,
+                        FILE_KEY,
+                    new File(filePath)
+                );
+
+        verify(this.service).uploadObject(
+                BUCKET_NAME,
+                FILE_KEY,
+                new File(filePath)
+        );
+    }
+
+    @Test
+    public void whenCopyObjectFromS3Bucket_thenNotNull() {
+        this.service.copyObject(
+                BUCKET_NAME,
+                FILE_KEY,
+                BUCKET_NAME,
+                FILE_KEY
+        );
+
+        doReturn(new CopyObjectResult()).when(this.service).copyObject(
+                BUCKET_NAME,
+                FILE_KEY,
+                BUCKET_NAME,
+                FILE_KEY
+       );
+
+        verify(this.service).copyObject(
+                BUCKET_NAME,
+                FILE_KEY,
+                BUCKET_NAME,
+                FILE_KEY
+
+        );
+    }
+
+    @Test
+    public void whenDeleteObjectFromS3Bucket_thenCorrect() {
+        this.service.deleteObject(BUCKET_NAME, FILE_KEY);
+
+        doNothing().when(this.service).deleteObject(BUCKET_NAME, FILE_KEY);
+        verify(this.service).deleteObject(BUCKET_NAME, FILE_KEY);
+    }
+
+    @Test
+    public void whenDeleteObjectsFromS3Bucket_thenCorrect() {
+        List<KeyVersion> deleteObjects = List.of(new KeyVersion(FILE_KEY));
+
+        this.service.deleteObjects(BUCKET_NAME, deleteObjects);
+
+        doNothing().when(this.service).deleteObjects(BUCKET_NAME, deleteObjects);
+        verify(this.service).deleteObjects(BUCKET_NAME, deleteObjects);
     }
 }
