d015bf60a381b70ecfa00760b78b927ea38372f4
Camilo Villalba <72482325+camilosanz@users.noreply.github.com>
2022-12-05 22:12:25 -0500

    test: add remaining AWSS3ServiceIntegration tests

--- springboot-aws-s3-example/src/test/java/AWSS3ServiceIntegrationTest.java
+++ springboot-aws-s3-example/src/test/java/AWSS3ServiceIntegrationTest.java
@@ -1,4 +1,10 @@
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.CopyObjectResult;
+import com.amazonaws.services.s3.model.DeleteObjectsRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
+import com.amazonaws.services.s3.model.S3Object;
+
 import org.example.services.AWSS3Service;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,10 +14,16 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.*;
 
+import java.io.File;
+
 public class AWSS3ServiceIntegrationTest {
     private final String BUCKET_NAME = "bucket_name";
     private AmazonS3 s3;
     private AWSS3Service service;
+    private Bucket bucket;
+    private String objectKey = "object/key";
+    private File file;
+    private S3Object object;
 
     @Before
     public void setUp() {
@@ -31,5 +43,54 @@ public class AWSS3ServiceIntegrationTest {
         var result = this.service.isBucketExist(BUCKET_NAME);
         //verify(s3).doesBucketExistV2(BUCKET_NAME);
         Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void whenCreateBucket_thenReturnBucket() {
+        bucket = mock(Bucket.class);
+        when(this.s3.createBucket(anyString())).thenReturn(bucket);
+        Bucket result = this.service.createBucket(BUCKET_NAME);
+        Assertions.assertEquals(bucket, result);
+    }
+
+    @Test
+    public void whenUploadObject_thenReturnPutObjResult() {
+        file = mock(File.class);
+        PutObjectResult putObjectResult = mock(PutObjectResult.class);
+        when(this.s3.putObject(BUCKET_NAME, objectKey, file)).thenReturn(putObjectResult);
+        PutObjectResult result = this.service.uploadObject(BUCKET_NAME, objectKey, file);
+        Assertions.assertEquals(putObjectResult, result);
+    }
+
+    @Test
+    public void whenCopyObject_thenReturnCopyObjResult() {
+        String destinationObjKey = "dest/object/key";
+        String destinationBucketName = "dest_bucket_name";
+        CopyObjectResult copyObjectResult = mock(CopyObjectResult.class);
+        when(this.s3.copyObject(BUCKET_NAME, objectKey, destinationBucketName, destinationObjKey))
+            .thenReturn(copyObjectResult);
+        CopyObjectResult result = this.service.copyObject(BUCKET_NAME, objectKey, destinationBucketName, destinationObjKey);
+        Assertions.assertEquals(copyObjectResult, result);
+    }
+
+    @Test
+    public void whenDeleteObjectCalled_thenVerify() {
+        this.service.deleteObject(BUCKET_NAME, objectKey);
+        verify(this.s3, times(1)).deleteObject(BUCKET_NAME, objectKey);
+    }
+
+    @Test
+    public void whenGetObject_thenReturnObject() {
+        object = mock(S3Object.class);
+        when(this.s3.getObject(BUCKET_NAME, objectKey)).thenReturn(object);
+        S3Object result = this.service.getObject(BUCKET_NAME, objectKey);
+        Assertions.assertEquals(object, result);
+    }
+
+    @Test
+    public void whenDeleteObjectsCalled_thenVerify() {
+        String[] objectsKeys = new String[]{"object/key1", "object/key2"};
+        this.service.deleteObjects(BUCKET_NAME, objectsKeys);
+        verify(this.s3, times(1)).deleteObjects(any(DeleteObjectsRequest.class));        
     }
 }
