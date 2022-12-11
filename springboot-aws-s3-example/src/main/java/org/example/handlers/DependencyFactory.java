8570830dcf681fbdad342c136f48d490dca6355d
D13g0 <diego.jauregui.salvatierra@gmail.com>
2022-12-10 23:01:34 -0400

    refactor: using default credentials provider

--- springboot-aws-s3-example/src/main/java/org/example/handlers/DependencyFactory.java
+++ springboot-aws-s3-example/src/main/java/org/example/handlers/DependencyFactory.java
@@ -1,25 +1,17 @@
 package org.example.handlers;
 
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.AwsCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.lambda.LambdaClient;
 
-
 public class DependencyFactory {
     public static LambdaClient lambdaClient() {
-        // TODO add a default credential provider to invoke lambda
-        AwsCredentials cred = AwsBasicCredentials.create("<YOUR_ACCESS_KEY>", "<YOUR_SECRET_KEY>");
-
-        AwsCredentialsProvider credProvider = StaticCredentialsProvider.create(cred);
+        AwsCredentialsProvider credProvider = DefaultCredentialsProvider.create();
 
         return LambdaClient.builder()
-                // .credentialsProvider((AwsCredentialsProvider) new ProfileCredentialsProvider("default"))        
-                .credentialsProvider(credProvider)
+                .credentialsProvider(credProvider)        
                 .region(Region.US_EAST_1)
                 .httpClientBuilder(UrlConnectionHttpClient.builder())
                 .build();
