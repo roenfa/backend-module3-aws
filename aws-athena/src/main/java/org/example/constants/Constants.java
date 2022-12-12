f909f1756ad34b676fba6c53122fa8a73e8fcd49
Andres Galvis B <a.galvisbolivar@gmail.com>
2022-12-11 22:16:15 -0500

    fix: changing the bucket of athena

--- aws-athena/src/main/java/org/example/constants/Constants.java
+++ aws-athena/src/main/java/org/example/constants/Constants.java
@@ -1,9 +1,9 @@
 package org.example.constants;
 
 public interface Constants {
-    String ATHENA_OUTPUT_BUCKET = "s3://re-athena-s3/results/";
+    String ATHENA_OUTPUT_BUCKET = "s3://agb-athena-s3/results/";
     String ATHENA_SAMPLE_QUERY = "SELECT * FROM transactions;";
     int TIMEOUT = 100000;
     long SLEEP_AMOUNT_IN_MS = 1000;
-    String ATHENA_DEFAULT_DATABASE = "devopsathenadb";
+    String ATHENA_DEFAULT_DATABASE = "devopsandresgbathena";
 }
