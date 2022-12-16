16fb7a33709dc093e865f5fec74b68ad1d539aba
D13g0 <diego.jauregui.salvatierra@gmail.com>
2022-12-15 22:41:54 -0400

    feat: transaction controller, repository and bean configurations

--- aws-api-gateway/src/main/java/org/example/config/RepositoriesConfigurator.java
+++ aws-api-gateway/src/main/java/org/example/config/RepositoriesConfigurator.java
@@ -1,8 +1,11 @@
 package org.example.config;
 
 import org.example.models.Book;
+import org.example.models.Transaction;
 import org.example.repositories.BookRepository;
 import org.example.repositories.IBookRepository;
+import org.example.repositories.ITransactionRepository;
+import org.example.repositories.TransactionRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,5 +18,14 @@ public class RepositoriesConfigurator {
         books.save(new Book(2, "Running Spring in Serverless"));
 
         return books;
+    }
+    @Bean
+    public ITransactionRepository iTransactionRepository(){
+        java.sql.Date date = java.sql.Date.valueOf("2022-12-16");
+
+        var transactions = new TransactionRepository();
+        transactions.save(new Transaction("2163962193", "REFUND", 213213,date));
+
+        return transactions;
     }
 }
---
+++ aws-api-gateway/src/main/java/org/example/repositories/TransactionRepository.java
@@ -0,0 +1,35 @@
+package org.example.repositories;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.example.models.Transaction;
+
+public class TransactionRepository implements ITransactionRepository {
+  private List<Transaction> transactions;
+
+  public TransactionRepository(){
+    this.transactions = new ArrayList<>();
+  }
+
+  @Override
+  public Transaction save(Transaction transaction) {
+    this.transactions.add(transaction);
+    return transaction;
+  }
+
+  @Override
+  public Collection<Transaction> getAll() {
+    return this.transactions;
+  }
+
+  @Override
+  public Transaction getTransaction(String id) {
+    return transactions.stream()
+      .filter(transaction -> id == transaction.getId())
+      .findAny()
+      .orElse(null);
+  }
+
+}
