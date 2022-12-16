e5949a9a3097809b24f6d9727a154fef7c9c4128
Andres Galvis B <a.galvisbolivar@gmail.com>
2022-12-16 18:31:49 -0500

    Revert "Djag fp/feat/transaction controller"

--- aws-api-gateway/src/main/java/org/example/models/Transaction.java
+++ aws-api-gateway/src/main/java/org/example/models/Transaction.java
@@ -1,23 +1,2 @@
-
-package org.example.models;
-
-import java.sql.Date;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class Transaction {
-  private String id;
-  private String type;
-  private double amount;
-  private Date date;
-
-  public Transaction(String id, String type, double amount, Date date) {
-    this.id = id;
-    this.type = type;
-    this.amount = amount;
-    this.date = date;
-  }
+package org.example.models;public class Transaction {
 }
