7b88a78fd75253e6160fb213b8637419cdb2e9f7
D13g0 <diego.jauregui.salvatierra@gmail.com>
2022-12-19 00:49:21 -0400

    refactor: deploy scripts, template and dependencies

---
+++ aws-athena/undeploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eo pipefail
+ARTIFACT_BUCKET='dj-ag-final-project/artifacts'
+STACK_NAME='djag-athena-lambda'
+AWS_PROFILE=$1
+
+echo "Deleting stacks"
+aws cloudformation delete-stack --stack-name $STACK_NAME --profile $AWS_PROFILE
+echo "Deletion initiated, waiting until stack deletion is complete"
+aws cloudformation wait stack-delete-complete --stack-name $STACK_NAME --profile $AWS_PROFILE
+echo "Deletion completed"
