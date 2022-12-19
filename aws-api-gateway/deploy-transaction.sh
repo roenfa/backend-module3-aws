d8c888e59966b830a9d13b46b888d594970f6ad9
D13g0 <diego.jauregui.salvatierra@gmail.com>
2022-12-19 09:30:39 -0400

    refactor: add configurations for connection

--- aws-api-gateway/deploy-transaction.sh
+++ aws-api-gateway/deploy-transaction.sh
@@ -3,6 +3,7 @@ set -eo pipefail
 ARTIFACT_BUCKET='djag-api-gateway'
 TEMPLATE=stack-transaction.yml
 STACK_NAME='djag-api-gateway-lambda'
+FUNCTION_NAME='djag-api-gateway-lambda'
 AWS_PROFILE=$1
 
 # Spring Boot 2 stack
@@ -10,7 +11,7 @@ aws s3 cp build/distributions/aws-api-gateway-1.0-SNAPSHOT.zip s3://$ARTIFACT_BU
 aws cloudformation deploy --template-file $TEMPLATE --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
   apiGatewayName=lambda-springboot-api-djag \
-  functionName=lambda-spring-boot-function-djag \
+  functionName=$FUNCTION_NAME \
   functionHandler=org.example.handlers.LambdaTransactionHandler \
   s3BucketKey=aws-api-gateway-1.0-SNAPSHOT.zip --profile $AWS_PROFILE
 aws --region us-east-1 cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs" --output json --profile $AWS_PROFILE
