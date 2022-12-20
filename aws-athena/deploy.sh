a493eeb2622a37b3961da55e436262e542ee2242
D13g0 <diego.jauregui.salvatierra@gmail.com>
2022-12-19 20:29:16 -0400

    fix: athena configurations an profile

--- aws-athena/deploy.sh
+++ aws-athena/deploy.sh
@@ -7,11 +7,11 @@ AWS_PROFILE="default"
 REGION="us-east-1"
 
 # Spring Boot 2 stack
-aws s3 cp build/distributions/aws-athena-1.0-SNAPSHOT.zip s3://$ARTIFACT_BUCKET/aws-athena-1.0-SNAPSHOT.zip --profile $AWS_PROFILE  --region REGION
+aws s3 cp build/distributions/aws-athena-1.0-SNAPSHOT.zip s3://$ARTIFACT_BUCKET/aws-athena-1.0-SNAPSHOT.zip --profile $AWS_PROFILE  --region $REGION
 aws cloudformation deploy --template-file $TEMPLATE --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
   apiGatewayName=lambda-springboot-api \
-  functionName=lambda-spring-boot-function \
+  functionName=djag-athena-lambda \
   functionHandler=org.example.handlers.LambdaHandler \
-  s3BucketKey=aws-athena-1.0-SNAPSHOT.zip --profile $AWS_PROFILE --region REGION
-aws --region REGION cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs" --output json --profile $AWS_PROFILE
+  s3BucketKey=aws-athena-1.0-SNAPSHOT.zip --profile $AWS_PROFILE --region $REGION
+aws --region $REGION cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs" --output json --profile $AWS_PROFILE
